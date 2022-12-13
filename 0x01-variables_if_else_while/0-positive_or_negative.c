dc43c5c81dfabd2cee2b6e830e3f65fce388674b
Ernest Igwe <igweernest@gmail.com>
2022-12-13 08:08:37 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 /**
@@ -20,7 +21,7 @@ int main(void)
 	} else if (a == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, a);
-	} else
+	} else if (a < 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
